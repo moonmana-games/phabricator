d7e936981f88f1647967fe15a6fd5331bda1a06c
Aleksandr Maydaniuk <maydaniukoleksandr@gmail.com>
2021-11-03 21:37:28 +0200

    add patch for "role"
    add patch for "role"

---
+++ resources/sql/autopatches/001.releeph.role.sql
@@ -0,0 +1,21 @@
+USE `{$NAMESPACE}_releeph`;
+
+ SET NAMES utf8 ;
+
+ SET character_set_client = {$CHARSET} ;
+
+CREATE TABLE `releeph_role` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `dateCreated` int(10) unsigned NOT NULL,
+  `dateModified` int(10) unsigned NOT NULL,
+  `phid` varbinary(64) NOT NULL,
+  `name` varchar(128) CHARACTER SET {$CHARSET} COLLATE {$COLLATE_TEXT} NOT NULL,
+  `trunkBranch` varchar(255) CHARACTER SET {$CHARSET} COLLATE {$COLLATE_TEXT} NOT NULL,
+  `repositoryPHID` varbinary(64) NOT NULL,
+  `createdByUserPHID` varbinary(64) NOT NULL,
+  `isActive` tinyint(1) NOT NULL DEFAULT '1',
+  `details` longtext CHARACTER SET {$CHARSET} COLLATE {$COLLATE_TEXT} NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `roleName` (`name`),
+  UNIQUE KEY `key_phid` (`phid`)
+) ENGINE=InnoDB DEFAULT CHARSET={$CHARSET} COLLATE={$COLLATE_TEXT};
